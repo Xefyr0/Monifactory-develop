f00e203aea6159703f07d3d87ea986a93866638d
Xefyr0 <xefyr00@gmail.com>
2026-05-05 00:18:28 -0400

    Further tweaks to combustion fuel values

--- kubejs/server_scripts/fixes_tweaks/combustion_rebalancing.js
+++ kubejs/server_scripts/fixes_tweaks/combustion_rebalancing.js
@@ -8,26 +8,26 @@ ServerEvents.recipes(event => {
 
     event.recipes.gtceu.combustion_generator("naphtha")
         .inputFluids("gtceu:naphtha 1")
-        .duration(8)    // 20% less than default GT
+        .duration(7)    // 30% less than default GTM, roughly the same as GTNH
         .EUt(-GTValues.V[GTValues.LV])
 
     event.recipes.gtceu.combustion_generator("biodiesel")
         .inputFluids("gtceu:bio_diesel 1")
-        .duration(15)    // 87.5% more than default GT
+        .duration(15)    // 87.5% more than default GTM
         .EUt(-GTValues.V[GTValues.LV])
 
     event.recipes.gtceu.combustion_generator("diesel")
         .inputFluids("gtceu:diesel 1")
-        .duration(20)    // 33% more than default GT
+        .duration(20)    // 33% more than default GTM
         .EUt(-GTValues.V[GTValues.LV])
 
     event.recipes.gtceu.combustion_generator("cetane_diesel")
-        .inputFluids("gtceu:cetane_boosted_diesel 1")   // -50% cost compared to default GT
+        .inputFluids("gtceu:cetane_boosted_diesel 1")   // -50% cost compared to default GTM
         .duration(45)
         .EUt(-GTValues.V[GTValues.LV])
 
     event.recipes.gtceu.combustion_generator("rocket_fuel")
-        .inputFluids("gtceu:rocket_fuel 8")   // -50% cost compared to default GT
+        .inputFluids("gtceu:rocket_fuel 8")   // -50% cost compared to default GTM
         .duration(125)
         .EUt(-GTValues.V[GTValues.LV])
 })
