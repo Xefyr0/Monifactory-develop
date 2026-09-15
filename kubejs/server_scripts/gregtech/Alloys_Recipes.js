e1da576b02370a3ecb6f70924e85582c2687db50
Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
2026-09-15 11:50:06 -0400

    Add more Netherite alloying recipes and fix alloying recipe IDs (#2769)

--- kubejs/server_scripts/gregtech/Alloys_Recipes.js
+++ kubejs/server_scripts/gregtech/Alloys_Recipes.js
@@ -25,7 +25,7 @@ ServerEvents.recipes(event => {
         for (let itemA of inputA) {
             for (let itemB of inputB) {
                 // the a here is so that it gets sorted on top, jei sorts by recipe id
-                event.recipes.gtceu.alloy_smelter(`gtceu:${Item.of(itemA).idLocation.path}_and_${Item.of(itemB).idLocation.path}_into_${Item.of(result).idLocation.path}`)
+                event.recipes.gtceu.alloy_smelter(`gtceu:${Item.of(itemA).idLocation.path}_and_${Item.of(itemB).idLocation.path}_into_${Item.of(result.concat("_ingot")).idLocation.path}`)
                     .itemInputs(itemA, itemB)
                     .itemOutputs(Item.of(result.concat("_ingot")))
                     .duration(duration * 20)
@@ -84,18 +84,16 @@ ServerEvents.recipes(event => {
         ["3x #forge:dusts/steel", "3x #forge:ingots/steel"],
         "nuclearcraft:hard_carbon", 15, GTValues.VA[GTValues.HV], true);
 
+    alloySmeltingVariant(
+        ["4x #forge:dusts/gold", "4x #forge:ingots/gold"],
+        ["4x #forge:dusts/netherite_scrap", "4x #forge:ingots/netherite_scrap"],
+        "minecraft:netherite", 5, GTValues.VA[GTValues.LV], false);
+
     event.recipes.gtceu.alloy_smelter("kubejs:fission_reactor_glass")
         .itemInputs("nuclearcraft:fission_reactor_casing", "#forge:glass/colorless")
         .itemOutputs("nuclearcraft:fission_reactor_glass")
         .duration(50)
         .EUt(GTValues.VHA[GTValues.LV])
-
-    event.remove({ output: "minecraft:netherite_ingot" })
-    event.recipes.gtceu.alloy_smelter("kubejs:netherite_ingot")
-        .itemInputs("4x #forge:ingots/gold", "4x minecraft:netherite_scrap") // was flipped
-        .itemOutputs("1x minecraft:netherite_ingot")
-        .duration(100)
-        .EUt(GTValues.VA[GTValues.LV])
 })
 
 // Change base GT alloy recipes
