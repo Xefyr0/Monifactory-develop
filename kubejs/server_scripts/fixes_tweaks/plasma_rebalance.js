c9fb36b84e6bb2f473f900473ffb7e70d38c8760
WithersChat <76114055+WithersChat@users.noreply.github.com>
2026-09-22 15:03:52 +0200

    Further tweaks

--- kubejs/server_scripts/fixes_tweaks/plasma_rebalance.js
+++ kubejs/server_scripts/fixes_tweaks/plasma_rebalance.js
@@ -4,12 +4,12 @@
  */
 
 ServerEvents.recipes(event => {
-    // Nerf Helium Plasma EU per mB to 80% of its original value
+    // Nerf Helium Plasma EU per mB to 60% of its original value
     event.recipes.gtceu.plasma_generator("helium")
         .inputFluids("gtceu:helium_plasma 1")
         .outputFluids("gtceu:helium 1")
         .EUt(-GTValues.V[GTValues.EV])
-        .duration(1.6 * 20)
+        .duration(1.2 * 20)
 
     // Change plasma fusion recipes to input/output multiples of 144mB for metals instead of 16mB
     event.findRecipes({ type: "gtceu:fusion_reactor"}).forEach(recipe => {
